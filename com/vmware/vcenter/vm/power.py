705192680159378d15bc286ee2db8cd647c01a50
Steve Trefethen <strefethen@users.noreply.github.com>
2017-06-29 10:27:54 -0700

    Simpler example

--- com/vmware/vcenter/vm/power.py
+++ com/vmware/vcenter/vm/power.py
@@ -5,18 +5,7 @@ stub_config = vapiconnect.connect(server,
                                     username,
                                     password,
                                     skip_verification)
-atexit.register(vapiconnect.logout, stub_config)
-
-global vm
-vm = get_vm(stub_config, vm_name)
-if not vm:
-    raise Exception('Sample requires an existing vm with name ({}).'
-                    'Please create the vm first.'.format(vm_name))
 
 # Create Power stub used for making requests
 global vm_power_svc
 vm_power_svc = Power(stub_config)
-
-# Get the vm power state
-print('\n# Example: Get current vm power state')
-status = vm_power_svc.get(vm)
